8972335122e2418e4df53e8864c6a9ed39dd9ca4
FuzzyKittyz <topher.a@hotmail.com>
2023-02-24 10:58:16 -0700

    add new logo

--- brewery-react/src/App.js
+++ brewery-react/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
+import png from './wei.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={png} className="App-logo" alt="logo" />
         <p>
           REACT KING
         </p>
