2e358a4e0d38859f2570a1e3de9d6aa238957a05
FuzzyKittyz <topher.a@hotmail.com>
2023-02-24 11:00:27 -0700

    take away cool logo :(

--- brewery-react/src/App.js
+++ brewery-react/src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
-import png from './wei.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={png} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           REACT KING
         </p>
