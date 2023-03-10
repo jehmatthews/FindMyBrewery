cd75671a134f484a19ffe373ca025ccfe0deead5
FuzzyKittyz <topher.a@hotmail.com>
2023-03-10 15:49:01 -0700

    add delete for react app

--- brewery-react/src/components/UserFavs.js
+++ brewery-react/src/components/UserFavs.js
@@ -1,39 +1,59 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import '../styles/userfavs.scss';
+import "../styles/userfavs.scss";
 import Navigation from "./Navigation";
+import deleteBrewery from "../helpers/deleteBrewery";
 
 export default function UserFavBreweries() {
   const [favourites, setFavourites] = useState([]);
 
   useEffect(() => {
-    axios.get('/api/breweries/favourites')
-      .then(response => {
-        console.log('response', response)
-        setFavourites(response.data)
+    axios
+      .get("/api/breweries/favourites")
+      .then((response) => {
+        console.log("response", response);
+        setFavourites(response.data);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
       });
-  }, [])
+  }, []);
 
   return (
     <div className="user-fav-breweries">
       <Navigation />
-      {favourites.map(favourite => (
+      {favourites.map((favourite) => (
         <div key={favourite.id} className="brewery-card">
-          <div className="card-image" style={{ backgroundImage: `url(${favourite.image_url})` }}>
+          <div
+            className="card-image"
+            style={{ backgroundImage: `url(${favourite.image_url})` }}
+          >
             <div className="card-details">
-              <h4>{favourite.name}</h4>
+              <h4>
+                <a href={`/brewery/${favourite.id}`}>{favourite.name}</a>
+              </h4>
               <p>Brewery Location: {favourite.street}</p>
               <p>Brewery City: {favourite.city}</p>
               <p>Brewery State: {favourite.state}</p>
-              <p>Brewery Phone #: <a href={`tel:${favourite.phone}`}>{favourite.phone}</a></p>
-              <p>Website: <a href={favourite.website_url}>{favourite.website_url}</a></p>
+              <p>
+                Brewery Phone #:{" "}
+                <a href={`tel:${favourite.phone}`}>{favourite.phone}</a>
+              </p>
+              <p>
+                Website:{" "}
+                <a href={favourite.website_url}>{favourite.website_url}</a>
+              </p>
+              <button
+                onClick={() => {
+                  deleteBrewery(favourite.id);
+                }}
+              >
+                Delete Brewery
+              </button>
             </div>
           </div>
         </div>
       ))}
     </div>
-  )
+  );
 }
