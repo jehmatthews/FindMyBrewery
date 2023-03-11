fcee4333614b303626649d33fca716ef016a7271
jehmatthews <jehmatthews30@gmail.com>
2023-03-11 09:56:20 -0800

    add styling for random brewery

--- brewery-react/src/apiCalls/brewery-random.js
+++ brewery-react/src/apiCalls/brewery-random.js
@@ -16,57 +16,47 @@ function RandomBrewery() {
   }, []);
 
   return (
-    <div>
+    <div className="single-container">
       <Navigation />
-      <h1>Random Brewery!</h1>
-      <ul>
-        {breweries.map((brewery) => (
-          <li key={brewery.id}>
-            {" "}
-            <a href={`/brewery/${brewery.id}`}>{brewery.name}</a>
-            <p>Brewery Location: {brewery.street}</p>
-            <p>Brewery City: {brewery.city}</p>
-            <p>Brewery State: {brewery.state}</p>
-            <p>
-              Brewery Phone #:
-              <a href="tel:"> {brewery.phone}</a>
-            </p>
-            {brewery.website_url ? (
-              <p>
-                Brewery Website:
-                <a target="_blank" href={brewery.website_url}>
-                  {" "}
-                  {brewery.name}
-                </a>
-              </p>
-            ) : (
-              <p>
-                Brewery Website:
-                <a target="_blank" href={brewery.website_url}>
-                  {" "}
-                  Website Unavailable
-                </a>
-              </p>
-            )}
-            <button
-              onClick={() => {
-                insertBrewery(brewery.id);
-              }}
-            >
-              Add to Favourites
-            </button>
-            <button
-              onClick={() => {
-                window.location.reload();
-              }}
-            >
-              Randomize!
-            </button>
-          </li>
-        ))}
-      </ul>
+      <div className="brewery-card">
+        <h1 className="brewery-name">Random Brewery!</h1>
+        <ul className="brewery-details">
+          {breweries.map((brewery) => (
+            <li key={brewery.id} className="brewery-details-item">
+              <a className="brewery-details-link" href={`/brewery/${brewery.id}`}>{brewery.name}</a>
+              <div className="brewery-details-group">
+                <span className="brewery-details-label">Location:</span>
+                <span className="brewery-details-value">{brewery.street}, {brewery.city}, {brewery.state}</span>
+              </div>
+              <div className="brewery-details-group">
+                <span className="brewery-details-label">Phone:</span>
+                <a className="brewery-details-value" href={`tel:${brewery.phone}`}>{brewery.phone}</a>
+              </div>
+              <div className="brewery-details-group">
+                <span className="brewery-details-label">Website:</span>
+                {brewery.website_url ? (
+                  <a className="brewery-details-link" target="_blank" href={brewery.website_url}>
+                    {brewery.name}
+                  </a>
+                ) : (
+                  <span className="brewery-details-unavailable">Website Unavailable</span>
+                )}
+              </div>
+              <div className="brewery-details-group">
+                <button className="add-to-favorites-button" onClick={() => { insertBrewery(brewery.id); }}>
+                  Add to Favorites
+                </button>
+                <button className="randomize-button" onClick={() => { window.location.reload(); }}>
+                  Randomize!
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
+  
 }
 
 export default RandomBrewery;
